7d33fce8f0ad5cd4d4bf8ccaf38a8afa69541de8
shiva807 <49604032+shiva807@users.noreply.github.com>
2020-10-27 11:53:25 +0530

    prog 1 completed

---
+++ LAB 4/dsr-lab4-prog1_code.R
@@ -0,0 +1,10 @@
+#boxplot
+count<-c(9, 25, 15, 2, 14, 25, 24, 47)
+speed<-c(2, 3, 5, 9, 14, 24, 29, 34)
+fw<-data.frame(count, speed)
+fw
+
+boxplot(fw$count, fw$speed, Names=c('count', 'speed'), xlab='var', ylab='value', col='gray90')
+quantile(fw$count)
+quantile(fw$speed)
+IQR(fw$speed)
